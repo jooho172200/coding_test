6a1885dcea67d5016e11bd470a310b72b921439b
jooho172200 <77068635+jooho172200@users.noreply.github.com>
2025-03-26 19:21:08 +0900

    [Bronze V] Title: A/B, Time: 244 ms, Memory: 18652 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        
+        double p1= s.nextDouble();
+        double p2= s.nextDouble();
+        
+       
+        System.out.println(p1/p2);
+   
+    }
+}
