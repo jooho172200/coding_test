de4067cd9315570f4bf4af1775744dbc2bdf6bd4
jooho172200 <77068635+jooho172200@users.noreply.github.com>
2025-03-26 20:21:23 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+// #include <algorithm>
+// #include <queue>
+// #include <stack>
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    do
+    {
+        getline(cin, str);
+         cout << str<<endl;
+    }while(str!="");
+
+    return 0;
+}
