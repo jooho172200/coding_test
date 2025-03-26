4861c8454a89f8de80a3d0964eb9b29b7ad59c46
jooho172200 <77068635+jooho172200@users.noreply.github.com>
2025-03-26 20:07:34 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int x;
+    int sum = 0;
+    
+    for(int i=0; i<5; i++){
+        cin>>x;
+        sum+=x*x;
+    }
+    
+    cout<<sum%10;
+    
+    return 0;
+}
