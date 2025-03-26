b11517ef6359f0a3f7da30ae7c23554cc8cc6cf2
jooho172200 <77068635+jooho172200@users.noreply.github.com>
2025-03-26 14:17:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.84 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String name: completion){
+            map.put(name, map.getOrDefault(name,0)+1);
+        }
+        
+        for(String name : participant){
+            if(map.getOrDefault(name, 0) == 0){
+                return name;
+            }
+            
+            map.put(name, map.get(name)-1);
+        }
+        
+        return null;
+}
+}
