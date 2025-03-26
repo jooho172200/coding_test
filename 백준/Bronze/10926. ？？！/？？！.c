714be5bba40c42604972cbab68c8281aae8771aa
jooho172200 <77068635+jooho172200@users.noreply.github.com>
2025-03-26 20:19:24 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(){
+    char id[50] = {"joonas"};
+    char new_id[50];
+    
+    scanf("%s",new_id);
+    printf("%s?\?!",new_id);    
+
+    
+}
