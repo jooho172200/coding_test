90998e15a8e097edd21a6fd702fbcbb51d1fa7fc
jooho172200 <77068635+jooho172200@users.noreply.github.com>
2025-03-26 14:17:21 +0900

    [level 2] Title: 할인 행사, Time: 101.07 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        for(int i=0; i<want.length; i++){
+            map.put(want[i], number[i]);
+        }
+        
+        for(int i=0; i<discount.length - 9; i++){
+            HashMap<String, Integer> dis = new HashMap<>();
+            
+            for(int j=i; j<i+10; j++){
+                if(map.containsKey(discount[j])){
+                    dis.put(discount[j], dis.getOrDefault(discount[j],0) + 1);
+                }
+            }
+            
+            if(map.equals(dis)) answer++;
+        }
+        
+        
+        return answer;
+    }
+}
