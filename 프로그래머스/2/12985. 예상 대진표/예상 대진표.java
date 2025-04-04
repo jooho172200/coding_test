f2af5f2e42a9f19db6f96dd3f04a2b3acb21142d
jooho172200 <77068635+jooho172200@users.noreply.github.com>
2025-04-04 22:02:03 +0900

    Update 예상 대진표.java

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,7 +3,6 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer = 0;
-        int num = Math.abs(b-a);
         
         while(a!=b){
             a = (a+1)/2;
